34a16fdd041666a9567511adcc52842c1262275a
Kate Schlunz <schlunzk@gmail.com>
2017-11-12 20:07:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ function BoardMember(name, homeState,training){
    this.training=training
 
    this.veto = function(){
-     console.log("No, I must disagree")
+     return 'No, I must disagree'
    }
 
    this.approve=function(){
-     console.log("You can do that!")
+     return "You can do that!"
    }
 
    this.doCharity=function(){
-     console.log("I like to help people.")
+     return "I like to help people."
    }
 
    this.releasePressStatement=function(){
